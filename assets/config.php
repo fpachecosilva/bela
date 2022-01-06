f7a102a0f99cc60cc1c1bb8d0297750d80ac8575
unknown <fabio_pachecosilva@hotmail.com>
2022-01-06 18:12:42 -0300

    atualizando bela com ultimo update feito no pc

---
+++ assets/config.php
@@ -0,0 +1,28 @@
+
+<?php
+	$empresa = 'A BELA E A FERA';
+	$CNPJ = '00.000.000/0001-00';
+	$url = 'http://localhost/belaeafera/';	
+	// $url = 'https://abelaeaferaexperience.com.br/';
+	$images = $url.'assets/images/';
+	$Tags = '';
+	
+
+	// Alt Title 
+		$CompartilhamentoAltTitle = '';
+		$HomeAltTitle = '';
+		$SobreAltTitle = '';
+		$ContatoAltTitle = '';
+
+	// Social
+		$facebookPage = '';
+		$facebookid = '';
+		$instagram = 'abelaeaferaexperience/';
+		$youtube = '';
+
+	// Comprar Ingressos		
+		$siteVendasSP 		= 'https://olhaoingresso.showare.com.br/Performance/ShoWareFrontEndPerPerformance.aspx?display=list&eventid=208&websaleschannelkey=internetchaim&orig=bfsp';
+		$siteVendasRJ 		= 'https://olhaoingresso.showare.com.br/Performance/ShoWareFrontEndPerPerformance.aspx?eventid=232&filter=day&websaleschannelkey=internetchaim&orig=bfrj';
+		$siteVendasMG 		= 'https://olhaoingresso.showare.com.br/Performance/ShoWareFrontEndPerPerformance.aspx?eventid=260&filter=day&websaleschannelkey=internetchaim&orig=bfbh';
+		
+?>
