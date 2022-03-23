93b294db480b3d2f16b647b31dba338fd0e0855e
unknown <fabio_pachecosilva@hotmail.com>
2022-03-23 20:46:50 -0300

    adicionando page ingressos para convites gratuitos

---
+++ ingressos/assets/config.php
@@ -0,0 +1,26 @@
+
+<?php
+	$empresa = 'A BELA E A FERA';
+	$CNPJ = '00.000.000/0001-00';
+	$url = 'http://localhost/belaeafera/ingressos/';
+	// $url = 'https://abelaeaferaexperience.com.br/ingressos/';
+	$images = $url.'assets/images/';
+	$Tags = '';
+	
+
+	// Alt Title 
+		$CompartilhamentoAltTitle = '';
+		$HomeAltTitle = '';
+		$SobreAltTitle = '';
+		$ContatoAltTitle = '';
+
+	// Social
+		$facebookPage = '';
+		$facebookid = '';
+		$instagram = 'abelaeaferaexperience/';
+		$youtube = '';
+
+	// Comprar Ingressos		
+		$siteVendasRJ 		= 'https://olhaoingresso.showare.com.br/Performance/ShoWareFrontEndPerPerformance.aspx?display=list&eventid=321&websaleschannelkey=internetgratuidades';		
+		
+?>
