a20cc8eb4d5695450c0b3ebe57b9a3021ccbc8ef
unknown <fabio_pachecosilva@hotmail.com>
2022-09-15 18:35:15 -0300

    removendo sp e sorocaba

--- index.php
+++ index.php
@@ -76,51 +76,15 @@
 		<h1 class="">A BELA E A FERA EXPERIENCE</h1>
 
 		<div class="menu-locais-links">			
-			<a class="active" local="sp">SÃO PAULO</a>
-			<a class="" local="so">SOROCABA</a>
-			<a class="" local="mg">UBERLÂNDIA</a>
-			<a class="" local="rp">RIBEIRÃO PRETO</a>
+			<a class="active" local="go">GOIÂNIA</a>
 			<a class="" local="jp">JOÃO PESSOA</a>
-			<a class="" local="go">GOIÂNIA</a>
+			<a class="" local="rp">RIBEIRÃO PRETO</a>
 			<a class="" local="sc">SANTA CATARINA</a>
+			<a class="" local="mg">UBERLÂNDIA</a>
 			<a class="" local="ma">SÃO LUÍS</a>
 			<a class="" local="pr">CURITIBA</a>
-			<a class="" local="rs">PORTO ALEGRE</a>
 			<a class="" local="lopr">LONDRINA</a>
-		</div>
-		
-		<div class="local sp active">
-			<div class="wrapper-card">
-				<div class="card">
-					<div class="img-wrapper">
-						<img src="<? echo $images ?>icon02.png">
-					</div>
-					<h2>MORUMBI TOWN SHOPPING</h2>
-				</div>
-				<div class="card">
-					<div class="img-wrapper">
-						<img src="<? echo $images ?>icon03.png">
-					</div>
-					<h2>DURAÇÃO - 75min</h2>
-				</div>
-			</div>
-		</div>		
-
-		<div class="local so">
-			<div class="wrapper-card">
-				<div class="card">
-					<div class="img-wrapper">
-						<img src="<? echo $images ?>icon02.png">
-					</div>
-					<h2>SHOPPING IGUATEMI ESPLANADA</h2>
-				</div>
-				<div class="card">
-					<div class="img-wrapper">
-						<img src="<? echo $images ?>icon03.png">
-					</div>
-					<h2>DURAÇÃO - 75min</h2>
-				</div>
-			</div>
+			<a class="" local="rs">PORTO ALEGRE</a>
 		</div>
 
 		<div class="local mg">
@@ -174,7 +138,7 @@
 			</div>
 		</div>
 
-		<div class="local go">
+		<div class="local go active">
 			<div class="wrapper-card">
 				<div class="card">
 					<div class="img-wrapper">
@@ -278,122 +242,6 @@
 	</section>
 
 	<section id="valores">
-		<div class="local sp active">
-			<h1>HORÁRIOS E VALORES - SP</h1>
-			<h2>Quinta e Sexta - 18h e 20h <br>Sáb e Dom - 10h30, 13h, 15h, 17h, 19h e 21h</h2>			
-
-			<table class="tabela-b linha1">
-				<tr>
-					<td class="first" rowspan ='3'>Dias e Sessões</td>					
-				</tr>				
-				<tr>
-					<td class="first" colspan ='2'>SÁB - 10h30, 13h, 15h, 17h e 19h</td>
-				</tr>				
-				<tr>
-					<td class="first" colspan ='2'>DOM - 13h, 15h, 17h e 19h</td>
-				</tr>				
-				<tr>
-					<td class="acesso-normal">Acesso Normal</td>
-					<td>R$ 80,00 sendo R$ 45,00 Consumação + R$ 35,00 Couvert Artístico</td>
-				</tr>
-				<tr>
-					<td class="magic-pass">Magic Pass</td>
-					<td>R$ 95,00 sendo R$ 45,00 Consumação + R$ 50,00 Couvert Artístico com direito a MagicPass (acesso prioritário)</td>
-				</tr>				
-			</table>
-			<table class="tabela-b linha2">
-				<tr>
-					<td class="first" rowspan ='3'>Dias e Sessões</td>
-				</tr>				
-				<tr>
-					<td class="first" colspan ='2'>SÁB - 21h</td>
-				</tr>				
-				<tr>
-					<td class="first" colspan ='2'>DOM - 10h30 e 21h</td>
-				</tr>				
-				<tr>
-					<td class="acesso-normal">Acesso Normal</td>
-					<td>R$ 70,00 sendo R$ 45,00 Consumação + R$ 25,00 Couvert Artístico</td>
-				</tr>
-				<tr>
-					<td class="magic-pass">Magic Pass</td>
-					<td>R$ 80,00 sendo R$ 45,00 Consumação + R$ 35,00 Couvert Artístico com direito a MagicPass (acesso prioritário)</td>
-				</tr>				
-			</table>
-			<table class="tabela-b linha3">
-				<tr>
-					<td class="first">Dias e Sessões</td>
-					<td class="first">QUI e SEX - 18h e 20h</td>
-				</tr>
-				<tr>
-					<td class="acesso-normal">Acesso Normal</td>
-					<td>R$ 60,00 sendo R$ 45,00 Consumação + R$ 15,00 Couvert Artístico</td>
-				</tr>
-				<tr>
-					<td class="magic-pass">Magic Pass</td>
-					<td>R$ 70,00 sendo R$ 45,00 Consumação + R$ 25,00 Couvert Artístico com direito a MagicPass (acesso prioritário)</td>
-				</tr>
-			</table>
-		</div>		
-
-		<div class="local so">
-			<h1>HORÁRIOS E VALORES - SP</h1>
-			<h2>Quinta e Sexta - 18h e 20h <br>Sáb e Dom - 10h30, 13h, 15h, 17h, 19h e 21h</h2>			
-			
-			<table class="tabela-b linha1">
-				<tr>
-					<td class="first" rowspan ='3'>Dias e Sessões</td>					
-				</tr>				
-				<tr>
-					<td class="first" colspan ='2'>SÁB - 10h30, 13h, 15h, 17h e 19h</td>
-				</tr>				
-				<tr>
-					<td class="first" colspan ='2'>DOM - 13h, 15h, 17h e 19h</td>
-				</tr>				
-				<tr>
-					<td class="acesso-normal">Acesso Normal</td>
-					<td>R$ 80,00 sendo R$ 45,00 Consumação + R$ 35,00 Couvert Artístico</td>
-				</tr>
-				<tr>
-					<td class="magic-pass">Magic Pass</td>
-					<td>R$ 95,00 sendo R$ 45,00 Consumação + R$ 50,00 Couvert Artístico com direito a MagicPass (acesso prioritário)</td>
-				</tr>				
-			</table>
-			<table class="tabela-b linha2">
-				<tr>
-					<td class="first" rowspan ='3'>Dias e Sessões</td>
-				</tr>				
-				<tr>
-					<td class="first" colspan ='2'>SÁB - 21h</td>
-				</tr>				
-				<tr>
-					<td class="first" colspan ='2'>DOM - 10h30 e 21h</td>
-				</tr>				
-				<tr>
-					<td class="acesso-normal">Acesso Normal</td>
-					<td>R$ 70,00 sendo R$ 45,00 Consumação + R$ 25,00 Couvert Artístico</td>
-				</tr>
-				<tr>
-					<td class="magic-pass">Magic Pass</td>
-					<td>R$ 80,00 sendo R$ 45,00 Consumação + R$ 35,00 Couvert Artístico com direito a MagicPass (acesso prioritário)</td>
-				</tr>				
-			</table>
-			<table class="tabela-b linha3">
-				<tr>
-					<td class="first">Dias e Sessões</td>
-					<td class="first">QUI e SEX - 18h e 20h</td>
-				</tr>
-				<tr>
-					<td class="acesso-normal">Acesso Normal</td>
-					<td>R$ 60,00 sendo R$ 45,00 Consumação + R$ 15,00 Couvert Artístico</td>
-				</tr>
-				<tr>
-					<td class="magic-pass">Magic Pass</td>
-					<td>R$ 70,00 sendo R$ 45,00 Consumação + R$ 25,00 Couvert Artístico com direito a MagicPass (acesso prioritário)</td>
-				</tr>
-			</table>
-		</div>
-
 		<div class="local mg">
 			<h1>HORÁRIOS E VALORES - MG</h1>
 			<h2>Quinta e Sexta - 18h e 20h <br>Sáb e Dom - 10h30, 13h, 15h, 17h, 19h e 21h</h2>			
@@ -568,7 +416,7 @@
 			</table>
 		</div>
 
-		<div class="local go">
+		<div class="local go active">
 			<h1>HORÁRIOS E VALORES - GO</h1>
 			<h2>Quinta e Sexta - 18h e 20h <br>Sáb e Dom - 10h30, 13h, 15h, 17h, 19h e 21h</h2>
 						
@@ -1112,26 +960,8 @@
 		
 		<div class="wrapper-card">
 			<div class="card">
-				<a onclick="gtag_report_conversion('<? echo $siteVendasSP ?>')" class="Btn" href="<? echo $siteVendasSP ?>">
-					<h2>São Paulo</h2>
-				</a>
-			</div>			
-
-			<div class="card">
-				<a onclick="gtag_report_conversion('<? echo $siteVendasSO ?>')" class="Btn" href="<? echo $siteVendasSO ?>">
-					<h2>Sorocaba</h2>
-				</a>
-			</div>
-
-			<div class="card">
-				<a onclick="gtag_report_conversion('<? echo $siteVendasMG ?>')" class="Btn" href="<? echo $siteVendasMG ?>">
-					<h2>Uberlândia</h2>
-				</a>
-			</div>
-
-			<div class="card">
-				<a onclick="gtag_report_conversion('<? echo $siteVendasRP ?>')" class="Btn" href="<? echo $siteVendasRP ?>">
-					<h2>Ribeirão Preto</h2>
+				<a onclick="gtag_report_conversion('<? echo $siteVendasGO ?>')" class="Btn" href="<? echo $siteVendasGO ?>">
+					<h2>Goiânia</h2>
 				</a>
 			</div>
 
@@ -1142,14 +972,20 @@
 			</div>
 
 			<div class="card">
-				<a onclick="gtag_report_conversion('<? echo $siteVendasGO ?>')" class="Btn" href="<? echo $siteVendasGO ?>">
-					<h2>Goiânia</h2>
+				<a onclick="gtag_report_conversion('<? echo $siteVendasRP ?>')" class="Btn" href="<? echo $siteVendasRP ?>">
+					<h2>Ribeirão Preto</h2>
 				</a>
 			</div>
 
 			<div class="card">
 				<a onclick="gtag_report_conversion('<? echo $siteVendasSC ?>')" class="Btn" href="<? echo $siteVendasSC ?>">
 					<h2>Florianópolis</h2>
+				</a>
+			</div>
+
+			<div class="card">
+				<a onclick="gtag_report_conversion('<? echo $siteVendasMG ?>')" class="Btn" href="<? echo $siteVendasMG ?>">
+					<h2>Uberlândia</h2>
 				</a>
 			</div>
 
@@ -1166,14 +1002,14 @@
 			</div>
 
 			<div class="card">
-				<a onclick="gtag_report_conversion('<? echo $siteVendasRS ?>')" class="Btn" href="<? echo $siteVendasRS ?>">
-					<h2>Porto Alegre</h2>
+				<a onclick="gtag_report_conversion('<? echo $siteVendasLOPR ?>')" class="Btn" href="<? echo $siteVendasLOPR ?>">
+					<h2>Londrina</h2>
 				</a>
 			</div>
 
 			<div class="card">
-				<a onclick="gtag_report_conversion('<? echo $siteVendasLOPR ?>')" class="Btn" href="<? echo $siteVendasLOPR ?>">
-					<h2>Londrina</h2>
+				<a onclick="gtag_report_conversion('<? echo $siteVendasRS ?>')" class="Btn" href="<? echo $siteVendasRS ?>">
+					<h2>Porto Alegre</h2>
 				</a>
 			</div>
 		</div>
