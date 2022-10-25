435e0e4916e02c075c658b65b8a207fe04f8ebe8
unknown <fabio_pachecosilva@hotmail.com>
2022-10-24 21:38:50 -0300

    removendo floripa

--- index.php
+++ index.php
@@ -74,30 +74,12 @@
 		<h1 class="">A BELA E A FERA EXPERIENCE</h1>
 
 		<div class="menu-locais-links">			
-			<a class="active" local="sc">SANTA CATARINA</a>			
-			<a class="" local="pr">CURITIBA</a>			
+			<a class="active" local="pr">CURITIBA</a>			
 			<a class="" local="rs">PORTO ALEGRE</a>
 			<a class="" local="ma">SÃO LUÍS</a>
 		</div>
 
-		<div class="local sc active">
-			<div class="wrapper-card">
-				<div class="card">
-					<div class="img-wrapper">
-						<img src="<?php echo $images ?>icon02.png">
-					</div>
-					<h2>FLORIPA SHOPPING</h2>
-				</div>
-				<div class="card">
-					<div class="img-wrapper">
-						<img src="<?php echo $images ?>icon03.png">
-					</div>
-					<h2>DURAÇÃO - 75min</h2>
-				</div>
-			</div>
-		</div>
-
-		<div class="local pr">
+		<div class="local pr active">
 			<div class="wrapper-card">
 				<div class="card">
 					<div class="img-wrapper">
@@ -150,102 +132,7 @@
 	</section>
 
 	<section id="valores">
-		<div class="local sc active">
-			<h1>HORÁRIOS E VALORES - SC</h1>
-			<h2>Quinta e Sexta - 18h e 20h <br>Sáb e Dom - 10h30, 13h, 15h, 17h, 19h e 21h</h2>
-
-			<div class="modelo-3">
-				<table>
-					<tr>
-						<td class="cel-amarela">Dias e Sessões</td>
-						<td class="cel-amarela">QUI e SEX - 16h, 18h e 20h</td>
-					</tr>
-					<tr>
-						<td class="cel-azul">Magic Fast Pass (Acesso Exclusivo)</td>
-						<td class="cel-verde">Acesso Normal</td>
-					</tr>
-					<tr>
-						<td>Combo VIP</td>
-						<td>Combo VIP</td>
-					</tr>
-					<tr>
-						<td>Show, Entrada e Prato Principal</td>
-						<td>Show, Entrada e Prato Principal</td>
-					</tr>
-					<tr>
-						<td>De R$ 104,90 por R$ 74,90</td>
-						<td>De R$ 94,90 por R$ 64,90</td>
-					</tr>
-				</table>
-				
-				<table>
-					<tr>
-						<td class="cel-amarela" rowspan="2" colspan="2">Dias e Sessões</td>
-						<td class="cel-amarela" colspan="2">SÁB - 21h</td>
-					</tr>
-					<tr>
-						<td class="cel-amarela" colspan="2">DOM - 19h e 21h</td>
-					</tr>
-					<tr>
-						<td class="cel-azul" colspan="2">Magic Fast Pass (Acesso Exclusivo)</td>
-						<td class="cel-verde" colspan="2">Acesso Normal</td>
-					</tr>
-					<tr class="colunas-4">
-						<td>Combo Premium</td>
-						<td>Combo VIP</td>
-						<td>Combo Premium</td>
-						<td>Combo VIP</td>
-					</tr>
-					<tr class="colunas-4">
-						<td>Show, Entrada, Prato Principal e Sobremesa</td>
-						<td>Show, Entrada, Prato Principal</td>
-						<td>Show, Entrada, Prato Principal e Sobremesa</td>
-						<td>Show, Entrada, Prato Principal</td>
-					</tr>
-					<tr class="colunas-4">
-						<td>De R$ 119,90 por R$ 89,90</td>
-						<td>R$ 79,90</td>
-						<td>De R$ 109,90 por R$ 79,90</td>
-						<td>R$ 69,90</td>
-					</tr>
-				</table>
-
-				<table>
-					<tr>
-						<td class="cel-amarela" rowspan="2" colspan="2">Dias e Sessões</td>
-						<td class="cel-amarela" colspan="2">SÁB - 13h, 15h, 17h e 19h</td>
-					</tr>
-					<tr>
-						<td class="cel-amarela" colspan="2">DOM - 13h, 15h, 17h</td>
-					</tr>
-					<tr>
-						<td class="cel-azul" colspan="2">Magic Fast Pass (Acesso Exclusivo)</td>
-						<td class="cel-verde" colspan="2">Acesso Normal</td>
-					</tr>
-					<tr class="colunas-4">
-						<td>Combo Premium</td>
-						<td>Combo VIP</td>
-						<td>Combo Premium</td>
-						<td>Combo VIP</td>
-					</tr>
-					<tr class="colunas-4">
-						<td>Show, Entrada, Prato Principal e Sobremesa</td>
-						<td>Show, Entrada, Prato Principal</td>
-						<td>Show, Entrada, Prato Principal e Sobremesa</td>
-						<td>Show, Entrada, Prato Principal</td>
-					</tr>
-					<tr class="colunas-4">
-						<td>De R$ 129,90 por R$ 99,90</td>
-						<td>R$ 89,90</td>
-						<td>De R$ 119,90 por R$ 89,90</td>
-						<td>R$ 79,90</td>
-					</tr>
-				</table>
-
-			</div>
-		</div>		
-
-		<div class="local pr">
+		<div class="local pr active">
 			<h1>HORÁRIOS E VALORES - PR</h1>
 			<h2>Quinta e Sexta - 18h e 20h <br>Sáb e Dom - 10h30, 13h, 15h, 17h, 19h e 21h</h2>
 
@@ -540,12 +427,6 @@
 		</p>
 		
 		<div class="wrapper-card">
-			<div class="card">
-				<a onclick="gtag_report_conversion('<?php echo $siteVendasSC ?>')" class="Btn" href="<?php echo $siteVendasSC ?>">
-					<h2>Florianópolis</h2>
-				</a>
-			</div>
-
 			<div class="card">
 				<a onclick="gtag_report_conversion('<?php echo $siteVendasPR ?>')" class="Btn" href="<?php echo $siteVendasPR ?>">
 					<h2>Curitiba</h2>
